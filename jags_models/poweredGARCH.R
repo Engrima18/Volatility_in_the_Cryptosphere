e778abfaf4b73a2c07b570ed5f0384e1961cb048
Enrico Grimaldi <engrima2000@gmail.com>
2023-08-16 18:27:46 +0200

    tentative powered model

---
+++ jags_models/poweredGARCH.R
@@ -0,0 +1,47 @@
+library(R2jags)
+source("functions.R")
+
+y <- data$LogReturns
+v <- data$Volume
+N <- length(y)
+
+# Jags code to fit the model to the simulated data
+tstud_model_code <- "
+model
+{
+  # Likelihood
+  for (t in 1:N) {
+    y[t] ~ dt(mu, tau[t], 8)
+    v[t] ~ dnorm(mu, tau[t])
+    tau[t] <- 1/pow(sigma[t], 2)
+  }
+  sigma[1] ~ dunif(0,10)
+  for(t in 2:N) {
+    sigma[t] <- sqrt(omega + alpha * pow(y[t-1] - mu, 2) + beta * pow(sigma[t-1], 2))
+  }
+
+  # Priors
+  mu ~ dnorm(0.0, 0.01)
+  omega ~ dunif(0, 10)
+  alpha ~ dunif(0, 1)
+  beta ~ dunif(0, 1)
+  gamma ~ dunif(0, 1)
+}
+"
+
+# Set up the data
+tstud_model_data <- list(N = N, y = y)
+
+# Choose the parameters to watch
+tstud_model_parameters <- c("omega", "alpha", "beta", "mu")
+
+# Run the model
+tstud_garch_model <- jags(
+  data = model_data,
+  parameters.to.save = model_parameters,
+  model.file = textConnection(model_code),
+  n.chains = 3, # Number of different starting positions
+  n.iter = 1000, # Number of iterations
+  n.burnin = 200, # Number of iterations to remove at start
+  n.thin = 2
+)
